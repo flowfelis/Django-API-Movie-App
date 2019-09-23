5cc3efdcac998a112d0b648f89bf89c500cd3fb0
puma <alicandonmez90@gmail.com>
2019-09-23 03:41:27 +0200

    Enable, Static file served via nginx

--- movieapi_project/settings_prod.py
+++ movieapi_project/settings_prod.py
@@ -1,3 +1,3 @@
 from .settings import *
 
-ALLOWED_HOSTS = ['movieapi.flowfelis.com', 'www.movieapi.flowfelis.com']
+ALLOWED_HOSTS = ['movieapi.flowfelis.com', 'www.movieapi.flowfelis.com', '67.207.77.115']
